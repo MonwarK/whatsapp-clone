46cfc661ac06f841bf29962740fec5d61aeb16ac
Monwar <monwar259@gmail.com>
2021-07-01 16:54:10 +0100

    changed image types

--- components/Loading.js
+++ components/Loading.js
@@ -1,10 +1,18 @@
 import { Circle } from "better-react-spinkit"
+import Image from "next/image"
 
 export default function Loading() {
     return (
         <center className="min-h-screen grid place-items-center">
             <div>
-                <img className="w-48 h-48 mb-14" src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c543.png" alt="Logo" />
+                <div className="mb-14">
+                    <Image 
+                        width={192}
+                        height={192}
+                        src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c543.png"
+                        alt="Logo"
+                    />
+                </div>
                 <Circle color="#3CBC28" size={60} />
             </div>
         </center>
--- pages/login.js
+++ pages/login.js
@@ -1,5 +1,6 @@
 import { Button } from "@material-ui/core";
 import { auth, provider } from "../firebase";
+import Image from "next/image"
 
 export default function login() {
 
@@ -12,7 +13,14 @@ export default function login() {
     return (
         <div className="grid place-items-center h-screen bg-gray-100">
             <div className="bg-white shadow-lg p-20">
-                <img className="w-48 h-48 mb-14" src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c543.png" alt="Logo" />
+                <div className="mb-14">
+                    <Image 
+                        width={192}
+                        height={192}
+                        src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c543.png"
+                        alt="Logo"
+                    />
+                </div>
                 <Button variant="outlined" onClick={signIn}>Sign in with Google</Button>
             </div>
         </div>
